976b34bf1e3dcd75c74add4c26e39e6426381256
Ubuntu <duleba.pio@gmail.com>
2020-05-20 13:43:44 -0700

    fixed communicaion

--- Raspberry/server.py
+++ Raspberry/server.py
@@ -37,35 +37,3 @@ while True:
             message = conn.recv(1024)
             print(str(message.decode("utf-8") ))
 
-
-# while True:
-#     #enter command
-#     cmd = input('>>')+'\n'
-#     #send command to client(Raspberry)
-#     conn.send(cmd.encode())
-#     #read&print result message after executing command
-#     message = conn.recv(1024)
-#     print(str(message.decode("utf-8") ))
-    
-    
-    
-# while True:
-#     read_list = [server, device_connection]
-#     read_ready, write_ready, err = select.select(read_list,[],[])
-#     for source in read_ready:
-#         if source == server:
-#             #send cmd to device
-#             print("----------------------------------------------------------------------")
-#             message = server.recv(1024)
-#             print("Received from server: "+  str(message.decode()))
-#             device_connection.send(message.encode(encoding='UTF-8'))
-#             print("Sent to device: "+  str(message.decode()))
-#         elif source == device_connection:
-#             #send msg to server
-#             print("----------------------------------------------------------------------")
-#             message = device_connection.recv(999)
-#             print("Received from device: "+  str(message.decode()))
-#             server.send(message)
-#             print("Sent to server: "+str(message.decode("utf-8") ))
-
-    
--- Raspberry/test.py
+++ Raspberry/test.py
@@ -59,22 +59,6 @@ while True:
 
 
 
-# while True:
-#     try:         
-#             #listen for message from server
-#             message = server.recv(1024)
-#             if message:
-#                 cmd = message.decode()
-#                 #send message to device
-#                 device_connection.send(cmd.encode(encoding='UTF-8'))
-#                 time.sleep(1)
-#                 #read response from device after executing command
-#                 res = device_connection.recv(999)
-#                 #send response to server
-#                 server.send(res)
-#                 print(str(res.decode("utf-8") ))
-#     except:
-#         continue
 
   
 ssh_client.close 
