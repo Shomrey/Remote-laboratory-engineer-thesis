d7dd0ee0a465ed266ef056722d732d7c8431ebfc
Ubuntu <duleba.pio@gmail.com>
2020-06-17 12:22:11 -0700

    added database to server

---
+++ Server/Python/data_generator.py
@@ -0,0 +1,33 @@
+
+import database
+import random
+
+import csv
+
+def add_students():
+    with open('Students.csv', newline='') as f:
+        reader = csv.reader(f)
+        datas = list(reader)
+    for data in datas:
+        database.add_student(int(data[0]),data[1],data[2],data[3],data[4])
+
+def add_teachers():
+    with open('Teachers.csv', newline='') as f:
+        reader = csv.reader(f)
+        datas = list(reader)
+    for data in datas:
+        database.add_teacher(int(data[0]),data[1],data[2],data[3],data[4])
+
+
+def add_labs():
+    with open('Labs.csv', newline='') as f:
+        reader = csv.reader(f)
+        datas = list(reader)
+    for data in datas:
+        database.add_laboratory(int(data[0]),data[1],int(data[2]),data[3],data[4],data[5],data[6],int(data[7]),int(data[8]))
+
+def add_enrollments():
+    #jak jest inna liczba studentw ni 100 i inna liczba labow niz 10 to tutaj trzeba zmienic
+    for i in range(1, 100):
+        database.enroll_student(i,random.randint(1,9))
+
---
+++ Server/Python/database.py
@@ -0,0 +1,85 @@
+import sqlite3
+import csv
+conn = sqlite3.connect('database.db')
+c = conn.cursor()
+
+def create_tables():
+    global c
+    #Tabela studentow
+    c.execute('''CREATE TABLE Students (id integer PRIMARY KEY, name text , surname text , mail text,  password text NOT NULL)''')
+    conn.commit()
+    #Tabela nauczycieli
+    c.execute('''CREATE TABLE Teachers (id integer PRIMARY KEY, name text, surname text, mail text, password text NOT NULL)''')
+    conn.commit()
+    #Tabela labow
+    c.execute('''CREATE TABLE Labs (id integer PRIMARY KEY,date text,duration integer, title text, configuration text, description text, topology blob, max_students integer DEFAULT 1,teacher integer, FOREIGN KEY(teacher) REFERENCES Teachers(id))''')
+    conn.commit()
+    #Tabela zapisow studentow na pojedyncze laby
+    c.execute('''CREATE TABLE Enrollments (id integer PRIMARY KEY AUTOINCREMENT, student integer, laboratory integer, result text, score int, comment text,FOREIGN KEY(student) REFERENCES Students(id),FOREIGN KEY(laboratory) REFERENCES Labs(id) )''')
+    conn.commit()
+   
+
+def add_student(id:int, name:str, surname:str, mail: str, password: str)-> bool:
+    global c
+    sql = ''' INSERT INTO Students(id, name, surname, mail, password) VALUES (?,?,?,?,?)'''
+    
+    try:
+        c.execute(sql, (id,name, surname, mail, password))     
+        conn.commit()
+        return True
+    except:
+        print("Failed to add student")
+        return False
+
+def add_teacher(id:int, name:str, surname:str, mail: str, password: str)-> bool:
+    global c
+    sql = ''' INSERT INTO Teachers(id, name, surname, mail, password) VALUES (?,?,?,?,?)'''
+    
+    try:
+        c.execute(sql, (id,name, surname, mail, password))     
+        conn.commit()
+        return True
+    except:
+        print("Failed to add teacher")
+        return False
+
+def add_laboratory(id:int, date:str, duration:int, title:str, configuration:str, description:str, topology, max_students:int, teacher:int )->bool:
+    global c
+    check = c.execute('SELECT * FROM Teachers WHERE id=?', (teacher,))
+    if len(check.fetchall())==0:
+        print("Failed to add library, no teacher with id: "+str(teacher))
+        return False
+
+    sql = ''' INSERT INTO Labs(id, date,duration, title, configuration, description, topology, max_students, teacher) VALUES(?,?,?,?,?,?,?,?,?)'''
+    try:
+        c.execute(sql, (id,date, duration, title, configuration, description,topology,max_students, teacher))     
+        conn.commit()
+        return True
+    except:
+        print("Failed to add laboratory")
+        return False
+
+def enroll_student(student_id: int, lab_id:int)->bool:
+    global c
+    check_student = c.execute('SELECT * FROM Students WHERE id=?', (student_id,))
+    if len(check_student.fetchall())==0:
+        print("Failed to enroll, no student with id: "+str(student_id))
+        return False
+    check_lab = c.execute('SELECT * FROM Labs WHERE id=?', (lab_id,))
+    if len(check_lab.fetchall())==0:
+        print("Failed to enroll, no laboratory with id: "+str(lab_id))
+        return False
+
+    sql = ''' INSERT INTO Enrollments (student, laboratory) VALUES(?,?)'''
+    try:
+        c.execute(sql, (student_id, lab_id))     
+        conn.commit()
+        return True
+    except:
+        print("Failed to add enrollment")
+        return False
+
+
+
+
+
