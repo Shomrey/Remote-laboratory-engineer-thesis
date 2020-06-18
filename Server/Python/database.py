1ab68dda8882c4b48b4cd5a29414fad10aa91d5a
meehaw1337-laptop <15misiek12@wp.pl>
2020-06-18 13:54:31 +0200

    Logging in and registration API added using flask, some refactoring"

--- Server/Python/database.py
+++ Server/Python/database.py
@@ -1,79 +1,93 @@
 import sqlite3
 import csv
+import sys
 
-conn = sqlite3.connect('database.db')
+# Not sure if safe... but doesn't work otherwise
+conn = sqlite3.connect('database.db', check_same_thread=False)
+conn.row_factory = sqlite3.Row
 c = conn.cursor()
+
 
 def create_tables():
     global c
-    #Tabela studentow
-    c.execute('''CREATE TABLE Students (id integer PRIMARY KEY, name text , surname text , mail text UNIQUE,  password_hash text NOT NULL)''')
+    # Students table
+    c.execute('''CREATE TABLE IF NOT EXISTS Students (id integer PRIMARY KEY AUTOINCREMENT, name text , surname text , mail text UNIQUE,  password_hash text NOT NULL)''')
     conn.commit()
-    #Tabela nauczycieli
-    c.execute('''CREATE TABLE Teachers (id integer PRIMARY KEY, name text, surname text, mail text UNIQUE, password_hash text NOT NULL)''')
-    conn.commit()
-    #Tabela labow
-    c.execute('''CREATE TABLE Labs (id integer PRIMARY KEY,date text,duration integer, title text, configuration text, description text, topology blob, max_students integer DEFAULT 1,teacher integer, FOREIGN KEY(teacher) REFERENCES Teachers(id))''')
-    conn.commit()
-    #Tabela zapisow studentow na pojedyncze laby
-    c.execute('''CREATE TABLE Enrollments (id integer PRIMARY KEY AUTOINCREMENT, student integer, laboratory integer, result text, score int, comment text,FOREIGN KEY(student) REFERENCES Students(id),FOREIGN KEY(laboratory) REFERENCES Labs(id) )''')
-    conn.commit()
-   
 
-def add_student(id:int, name:str, surname:str, mail: str, password_hash: str)-> bool:
+    # Teachers table
+    c.execute('''CREATE TABLE IF NOT EXISTS Teachers (id integer PRIMARY KEY AUTOINCREMENT, name text, surname text, mail text UNIQUE, password_hash text NOT NULL)''')
+    conn.commit()
+
+    # Labs table
+    c.execute('''CREATE TABLE IF NOT EXISTS Labs (id integer PRIMARY KEY AUTOINCREMENT,date text,duration integer, title text, configuration text, description text, topology blob, max_students integer DEFAULT 1,teacher integer, FOREIGN KEY(teacher) REFERENCES Teachers(id))''')
+    conn.commit()
+
+    # Enrollments table
+    c.execute('''CREATE TABLE IF NOT EXISTS Enrollments (id integer PRIMARY KEY AUTOINCREMENT, student integer, laboratory integer, result text, score int, comment text,FOREIGN KEY(student) REFERENCES Students(id),FOREIGN KEY(laboratory) REFERENCES Labs(id) )''')
+    conn.commit()
+
+
+def add_student(name: str, surname: str, mail: str, password_hash: str) -> bool:
     global c
-    sql = ''' INSERT INTO Students(id, name, surname, mail, password_hash) VALUES (?,?,?,?,?)'''
-    
+    sql = ''' INSERT INTO Students(name, surname, mail, password_hash) VALUES (?,?,?,?)'''
+
     try:
-        c.execute(sql, (id,name, surname, mail, password_hash))     
+        c.execute(sql, (name, surname, mail, password_hash))
         conn.commit()
+        print("Student", name, surname, mail, "added")
         return True
-    except:
-        print("Failed to add student")
+    except sqlite3.Error as e:
+        print("Failed to add student, database error:", e)
         return False
 
-def add_teacher(id:int, name:str, surname:str, mail: str, password_hash: str)-> bool:
+
+def add_teacher(name: str, surname: str, mail: str, password_hash: str) -> bool:
     global c
-    sql = ''' INSERT INTO Teachers(id, name, surname, mail, password_hash) VALUES (?,?,?,?,?)'''
-    
+    sql = ''' INSERT INTO Teachers(name, surname, mail, password_hash) VALUES (?,?,?,?)'''
+
     try:
-        c.execute(sql, (id,name, surname, mail, password_hash))     
+        c.execute(sql, (name, surname, mail, password_hash))
         conn.commit()
+        print("Teacher", name, surname, mail, "added")
         return True
     except:
         print("Failed to add teacher")
         return False
 
-def add_laboratory(id:int, date:str, duration:int, title:str, configuration:str, description:str, topology, max_students:int, teacher:int )->bool:
+
+def add_laboratory(date: str, duration: int, title: str, configuration: str, description: str, topology, max_students: int, teacher: int) -> bool:
     global c
     check = c.execute('SELECT * FROM Teachers WHERE id=?', (teacher,))
-    if len(check.fetchall())==0:
+    if len(check.fetchall()) == 0:
         print("Failed to add library, no teacher with id: "+str(teacher))
         return False
 
-    sql = ''' INSERT INTO Labs(id, date,duration, title, configuration, description, topology, max_students, teacher) VALUES(?,?,?,?,?,?,?,?,?)'''
+    sql = ''' INSERT INTO Labs(date,duration, title, configuration, description, topology, max_students, teacher) VALUES(?,?,?,?,?,?,?,?)'''
     try:
-        c.execute(sql, (id,date, duration, title, configuration, description,topology,max_students, teacher))     
+        c.execute(sql, (date, duration, title, configuration,
+                        description, topology, max_students, teacher))
         conn.commit()
         return True
     except:
         print("Failed to add laboratory")
         return False
 
-def enroll_student(student_id: int, lab_id:int)->bool:
+
+def enroll_student(student_id: int, lab_id: int) -> bool:
     global c
-    check_student = c.execute('SELECT * FROM Students WHERE id=?', (student_id,))
-    if len(check_student.fetchall())==0:
+    check_student = c.execute(
+        'SELECT * FROM Students WHERE id=?', (student_id,))
+    if len(check_student.fetchall()) == 0:
         print("Failed to enroll, no student with id: "+str(student_id))
         return False
     check_lab = c.execute('SELECT * FROM Labs WHERE id=?', (lab_id,))
-    if len(check_lab.fetchall())==0:
+    if len(check_lab.fetchall()) == 0:
         print("Failed to enroll, no laboratory with id: "+str(lab_id))
         return False
 
     sql = ''' INSERT INTO Enrollments (student, laboratory) VALUES(?,?)'''
     try:
-        c.execute(sql, (student_id, lab_id))     
+        c.execute(sql, (student_id, lab_id))
         conn.commit()
         return True
     except:
@@ -81,35 +95,54 @@ def enroll_student(student_id: int, lab_id:int)->bool:
         return False
 
 # VIEWS
-def get_teacher_labs(teacher_id:int)->list:
+
+
+def get_teacher_labs(teacher_id: int) -> list:
     global c
-    check_teacher = c.execute('SELECT * FROM Teachers WHERE id=?', (teacher_id,))
-    if len(check_teacher.fetchall())==0:
+    check_teacher = c.execute(
+        'SELECT * FROM Teachers WHERE id=?', (teacher_id,))
+    if len(check_teacher.fetchall()) == 0:
         print("Failed to read, no teacher with id: "+str(teacher_id))
         return None
-    result=c.execute('SELECT * FROM Labs WHERE teacher=?', (teacher_id,))
+    result = c.execute('SELECT * FROM Labs WHERE teacher=?', (teacher_id,))
     return result.fetchall()
 
 
-def get_students_from_lab(lab_id:int)->list:
+def get_students_from_lab(lab_id: int) -> list:
     global c
     check_lab = c.execute('SELECT * FROM Labs WHERE id=?', (lab_id,))
-    if len(check_lab.fetchall())==0:
+    if len(check_lab.fetchall()) == 0:
         print("Failed to read, no laboratory with id: "+str(lab_id))
         return None
-    result=c.execute('SELECT student FROM Enrollments WHERE laboratory=?', (lab_id,))
-    return result.fetchall()  
+    result = c.execute(
+        'SELECT student FROM Enrollments WHERE laboratory=?', (lab_id,))
+    return result.fetchall()
 
 
-
-def get_labs_for_student(student_id:int)->list:
+def get_labs_for_student(student_id: int) -> list:
     global c
-    check_student = c.execute('SELECT * FROM Students WHERE id=?', (student_id,))
-    if len(check_student.fetchall())==0:
+    check_student = c.execute(
+        'SELECT * FROM Students WHERE id=?', (student_id,))
+    if len(check_student.fetchall()) == 0:
         print("Failed to read, no student with id: "+str(student_id))
         return False
-    result=c.execute('SELECT laboratory FROM Enrollments WHERE student=?', (student_id,))
-    return result.fetchall()  
+    result = c.execute(
+        'SELECT laboratory FROM Enrollments WHERE student=?', (student_id,))
+    return result.fetchall()
 
 
-print(str(get_labs_for_student(12342)))
+def validate_student(mail: str, password: str):
+    global c
+    students = c.execute(
+        'SELECT * FROM Students WHERE mail=?', (mail,))
+    student = students.fetchone()
+    if student == None:
+        print("Failed to validate student, no student with mail: " + str(mail))
+        return False
+    else:
+        print(student)
+        if student['password_hash'] == password:
+            return True
+        else:
+            print('Passwords do not match')
+            return False
--- Server/Python/server.py
+++ Server/Python/server.py
@@ -1,6 +1,11 @@
 import socket
+import threading
+from flask import Flask, request
+from flask_api import status
+import database
 
-def run_server():
+
+def run_socket_server():
     server_ip = '127.0.0.1'
     mobile_port = 9010
     raspberry_port = 9011
@@ -11,25 +16,53 @@ def run_server():
     mobile_server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     raspberry_server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-
     response_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     response_socket.bind(('', response_port))
 
     #mobile_server_socket.bind(('', mobile_port))
     #raspberry_server_socket.bind(('', raspberry_port))
 
+    print('Server started, listening on', server_ip)
+    print('Ports:')
+    print('- mobile:', mobile_port)
+    print('- raspberry:', raspberry_port)
+    print('- response:', response_port)
+
     while True:
         mobile_buff, mobile_address = response_socket.recvfrom(1024)
 
-        response_socket.sendto(bytes(str(mobile_buff, 'cp1250'), 'cp1250'), (server_ip, raspberry_port))
+        response_socket.sendto(
+            bytes(str(mobile_buff, 'cp1250'), 'cp1250'), (server_ip, raspberry_port))
 
         raspberry_buff, raspberry_address = response_socket.recvfrom(1024)
 
-        response_socket.sendto(bytes(str(raspberry_buff, 'cp1250'), 'cp1250'), (server_ip, mobile_port))
+        response_socket.sendto(
+            bytes(str(raspberry_buff, 'cp1250'), 'cp1250'), (server_ip, mobile_port))
 
 
-run_server()
+database.create_tables()
+
+socketServerThread = threading.Thread(target=run_socket_server)
+socketServerThread.start()
+
+app = Flask(__name__)
 
 
+@app.route('/user/register', methods=['POST'])
+def register_user():
+    content = request.get_json()
+    # TODO content validation
+    if database.add_student(content['name'], content['surname'],
+                            content['mail'], content['password']):  # TODO hash the password
+        return 'Registered succesfully', status.HTTP_200_OK
+    else:
+        return 'Failed to register', status.HTTP_400_BAD_REQUEST
 
-
+@app.route('/user/login', methods=['POST'])
+def login():
+    content = request.get_json()
+    # TODO content validation
+    if database.validate_student(content['mail'], content['password']):  # TODO hash the password
+        return 'Login succesful', status.HTTP_200_OK
+    else:
+        return 'Failed to log in', status.HTTP_400_BAD_REQUEST
