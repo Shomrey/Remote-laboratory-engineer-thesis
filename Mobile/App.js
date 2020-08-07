fdcec55d95fd880f0ad41cfe90bd34342fbf13c9
Michał Komar <michal.komar@miquido.com>
2020-08-08 00:46:12 +0200

    Unused imports removed from app.js

--- Mobile/App.js
+++ Mobile/App.js
@@ -2,8 +2,6 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import Login from './Login'
 import {AuthProvider} from './AuthContext'
-import {useFonts} from "expo-font";
-import {AppLoading} from "expo";
 
 
 export default function App() {
