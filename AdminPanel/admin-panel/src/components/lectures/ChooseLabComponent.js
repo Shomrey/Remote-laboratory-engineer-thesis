5a9fdacabcfb54850676098f0ac3deb1253ba15c
Michał Przybycień <szachym1@gmail.com>
2020-11-15 17:41:09 +0100

    update lecture choice UI

--- AdminPanel/admin-panel/src/components/lectures/ChooseLabComponent.js
+++ AdminPanel/admin-panel/src/components/lectures/ChooseLabComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Button } from '@material-ui/core';
-import { Container } from '@material-ui/core';
+import { Container, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 
@@ -13,14 +13,41 @@ class ChooseLabComponent extends Component {
         this.props.handleChoice(index);
     }
     render() {
+        let columns = [{ field: 'firstname', headerName: 'First name', width: 50 },
+        { field: 'surname', headerName: 'Surname', width: 50 },
+        { field: 'mail', headerName: 'Email', width: 50 }];
         return (
             <Container>
-                <List>
-                    {this.props.labs.map((lecture, index) => <ListItem> <Button variant="contained" color="primary" id={index} onClick={() => this.handleClick(index)}>{lecture.title}</Button></ListItem>)}
-                </List>
+                <TableContainer>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell><strong>Lecture title</strong></TableCell>
+                                <TableCell><strong>Date</strong></TableCell>
+                                <TableCell><strong>Teacher</strong></TableCell>
+                                <TableCell><strong>Edit lecture details</strong></TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {this.props.labs.map((lecture, index) => (
+                                <TableRow key={index}>
+                                    <TableCell align="left">{lecture.title}</TableCell>
+                                    <TableCell align="left">{lecture.date}</TableCell>
+                                    <TableCell align="left">{lecture.teacher.name} {lecture.teacher.surname}</TableCell>
+                                    <TableCell align="left"> <Button variant="contained" color="primary" id={index} onClick={() => this.handleClick(index)}>Edit</Button></TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+
             </Container>
         );
     }
 }
+
+/*<List>
+                    {this.props.labs.map((lecture, index) => <ListItem> <Button variant="contained" color="primary" id={index} onClick={() => this.handleClick(index)}>{lecture.title}</Button></ListItem>)}
+                </List> */
 
 export default ChooseLabComponent;
