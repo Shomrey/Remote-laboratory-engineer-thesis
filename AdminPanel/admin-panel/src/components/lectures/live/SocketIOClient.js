7ff5994be37d67bb1a12a28cf95150d4bf6beed5
meehaw1337-PC <15misiek12@wp.pl>
2020-12-13 17:31:53 +0100

    socket.io-client version changed to 2.3.1

--- AdminPanel/admin-panel/src/components/lectures/live/SocketIOClient.js
+++ AdminPanel/admin-panel/src/components/lectures/live/SocketIOClient.js
@@ -1,42 +1,13 @@
-import React, { Component, useEffect, useState } from 'react';
-import { socketIOClient, io } from "socket.io-client";
+import React, { useEffect} from 'react';
+import socketIOClient from "socket.io-client";
 const ServerEndpoint = 'https://remote-laboratory.herokuapp.com'
 
-class Client extends Component {
-    state = {}
-
-    componentDidMount() {
-        const socket = io(ServerEndpoint);
-        socket.emit('admin');
-        console.log('emit');
-        socket.on('spy', data => console.log('message from socketio'));
-        socket.on("connect", () => {
-            console.log('connect');
-        });
-        socket.on("connect_error", () => {
-            console.log('error');
-        });
-    }
-
-    render() {
-        return (<div>client</div>);
-    }
-}
-
-export default Client;
-
-/*export default function Client(props) {
-    const [response, setResponse] = useState("");
-    console.log('Client creation');
-    const socket = socketIOClient(ServerEndpoint);
-    socket.emit('admin');
+export default function Client() {
     useEffect(() => {
-        console.log('useEffect');
-        socket.on("spy", data => {
-            console.log('Messge from admin');
-            console.log(data);
-        });
+        const socket = socketIOClient(ServerEndpoint);
+        socket.emit('admin')
+        socket.on('spy', data => console.log('message from socketio'));
     }, []);
 
-    return ('client')
-}*/
+    return (<div>client</div>);
+}
